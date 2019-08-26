7b72f9cd414c2539e15af5753db0b2836e428142
sjyan <yysijie@gmail.com>
2019-08-26 22:18:55 +0800

    update run.py

--- tools/run.py
+++ tools/run.py
@@ -7,15 +7,15 @@ import mmskeleton
 from mmskeleton.utils import call_obj
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Train a action recognizer')
-    parser.add_argument('config', help='train config file path')
+    parser = argparse.ArgumentParser(description='Run a processor')
+    parser.add_argument('config', help='processor config file path')
     args = parser.parse_args()
     return args
 
 def main():
     args = parse_args()
     cfg = Config.fromfile(args.config)
-    processor = call_obj(cfg.processor, cfg.processor_args)
+    call_obj(cfg.processor, cfg.processor_args)
 
 if __name__ == "__main__":
     main()
