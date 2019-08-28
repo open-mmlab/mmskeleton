f0046e33a0f3b642d1310901f8c76008ca345447
sjyan <yysijie@gmail.com>
2019-08-28 15:57:56 +0800

    add argparse configuration

--- tools/run.py
+++ tools/run.py
@@ -1,20 +1,37 @@
 import argparse
 import os
+import sys
 
 import torch
 from mmcv import Config
 import mmskeleton
 from mmskeleton.utils import call_obj
 
-def parse_args():
+
+
+def parse_cfg():
     parser = argparse.ArgumentParser(description='Run a processor')
     parser.add_argument('config', help='processor config file path')
+
+    # add argument from configuration file
+    cfg = Config.fromfile(sys.argv[1])
+    for key, info in cfg.argparse_cfg.items():
+        if 'bind_to' not in info:
+            continue
+        default = eval('cfg.{}'.format(info['bind_to']))
+        parser.add_argument('--' + key, default=default, help=info.get('help', None))
     args = parser.parse_args()
-    return args
+
+    # update config from command line
+    for key, info in cfg.argparse_cfg.items():
+        if 'bind_to' not in info:
+            continue
+        exec("cfg.{} = '{}'".format(info['bind_to'], getattr(args, key)))
+    
+    return cfg
 
 def main():
-    args = parse_args()
-    cfg = Config.fromfile(args.config)
+    cfg = parse_cfg()
     call_obj(**cfg.processor_cfg)
 
 if __name__ == "__main__":
