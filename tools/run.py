91fff27985fbc25d36807ec83a6b57f8266e5ff7
sjyan <yysijie@gmail.com>
2019-08-28 17:14:55 +0800

    smart getattr and setattr

--- tools/run.py
+++ tools/run.py
@@ -5,10 +5,10 @@ import sys
 import torch
 from mmcv import Config
 import mmskeleton
-from mmskeleton.utils import call_obj
+from mmskeleton.utils import call_obj, set_attr, get_attr
 
 
-""" Configuration Formation 
+""" Configuration Structure 
 
 argparse_cfg:
   <shortcut_name 1>:
@@ -28,26 +28,34 @@ def parse_cfg():
     parser = argparse.ArgumentParser(description='Run a processor')
     parser.add_argument('config', help='configuration file path')
 
-    # add argument from configuration file
+    # load argument setting from configuration file
     cfg = Config.fromfile(sys.argv[1])
     for key, info in cfg.argparse_cfg.items():
         if 'bind_to' not in info:
             continue
-        default = eval('cfg.{}'.format(info['bind_to']))
-        parser.add_argument('--' + key, default=default, help=info.get('help', None))
+        default = get_attr(cfg, info['bind_to'])
+        kwargs = dict(default=default, type=type(default))
+        kwargs.update({k:v for k,v in info.items() if k != 'bind_to'})
+        parser.add_argument('--' + key, **kwargs)
     args = parser.parse_args()
 
     # update config from command line
     for key, info in cfg.argparse_cfg.items():
         if 'bind_to' not in info:
             continue
-        exec("cfg.{} = '{}'".format(info['bind_to'], getattr(args, key)))
+        value = getattr(args, key)
+
+
+        if isinstance(value, str):
+            value = "'{}'".format(value)
+        set_attr(cfg, info['bind_to'], value)
     
     return cfg
 
 def main():
     cfg = parse_cfg()
-    call_obj(**cfg.processor_cfg)
+    print(cfg)
+    # call_obj(**cfg.processor_cfg)
 
 if __name__ == "__main__":
     main()
