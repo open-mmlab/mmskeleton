40ddcb600c1ac5f6dfb02ea525f2d2cc7b9fba1d
sjyan <yysijie@gmail.com>
2019-08-28 15:22:12 +0800

    st-gcn training

--- mmskeleton/processor/recognizer.py
+++ mmskeleton/processor/recognizer.py
@@ -6,70 +6,94 @@ from mmcv import Config
 from mmcv.parallel import MMDataParallel
 
 
-def parse_losses(losses):
-    log_vars = OrderedDict()
-    for loss_name, loss_value in losses.items():
-        if isinstance(loss_value, torch.Tensor):
-            log_vars[loss_name] = loss_value.mean()
-        elif isinstance(loss_value, list):
-            log_vars[loss_name] = sum(_loss.mean() for _loss in loss_value)
-        else:
-            raise TypeError(
-                '{} is not a tensor or list of tensors'.format(loss_name))
-
-    loss = sum(_value for _key, _value in log_vars.items() if 'loss' in _key)
-
-    log_vars['loss'] = loss
-    for name in log_vars:
-        log_vars[name] = log_vars[name].item()
-
-    return loss, log_vars
-
-
 def train(
     work_dir,
     model_cfg,
+    loss_cfg,
     dataset_cfg,
     optimizer_cfg,
     batch_size,
-    lr_config,
     total_epochs,
-    optimizer_config=None,
+    training_hooks,
+    workflow=[('train', 1)],
     gpus=1,
-    checkpoint_config=None,
-    log_config=None,
     log_level=0,
+    workers=4,
+    resume_from=None,
+    load_from=None,
 ):
-
     # prepare data loaders
-    dataset = call_obj(**dataset_cfg)
-    data_loader = torch.utils.data.DataLoader(
-        dataset=dataset,
+    if isinstance(dataset_cfg, dict):
+        dataset_cfg = [dataset_cfg]
+    data_loaders = [torch.utils.data.DataLoader(
+        dataset=call_obj(**d),
         batch_size=batch_size,
         shuffle=True,
-        num_workers=8,
-        drop_last=True)
-    data_loaders = [data_loader]
+        num_workers=workers,
+        drop_last=True) for d in dataset_cfg]
 
     # put model on gpus
-    model = call_obj(**model_cfg)
+    if isinstance(model_cfg, list):
+        model = [call_obj(**c) for c in model_cfg]
+        model = torch.nn.Sequential(*model)
+    else:
+        model = call_obj(**model_cfg)
+    model.apply(weights_init)
     model = MMDataParallel(model, device_ids=range(gpus)).cuda()
+    loss = call_obj(**loss_cfg)
+    models = torch.nn.ModuleList((model, loss))
 
     # build runner
     optimizer = call_obj(params=model.parameters(), **optimizer_cfg)
-    runner = Runner(model, batch_processor, optimizer, work_dir, log_level)
-    runner.register_training_hooks(lr_config, optimizer_config,
-                                   checkpoint_config, log_config)
+    runner = Runner(models, batch_processor, optimizer, work_dir, log_level)
+    runner.register_training_hooks(**training_hooks)
+
+    if resume_from:
+        runner.resume(resume_from)
+    elif load_from:
+        runner.load_checkpoint(load_from)
 
     # run
-    runner.run(data_loaders, [('train', 1)], total_epochs)
+    workflow = [tuple(w) for w in workflow]
+    runner.run(data_loaders, workflow, total_epochs)
 
+def batch_processor(models, datas, train_mode):
 
-def batch_processor(model, data, train_mode):
-    data, label = data
-    losses = model(data)
-    print(losses)
-    outputs = dict(
-        loss=loss, log_vars=log_vars, num_samples=len(data['img'].data))
+    data, label = datas
+    data = data.cuda()
+    label = label.cuda()
+    model, loss = models
 
+    # forward
+    output = model(data)
+    losses = loss(output, label)
+
+    # output
+    log_vars=dict(loss=losses.item())
+    if not train_mode:
+        log_vars['top1'] = topk_accuracy(output, label)
+        log_vars['top5'] = topk_accuracy(output, label, 5)
+
+    outputs = dict(loss=losses, log_vars=log_vars, num_samples=len(data.data))
     return outputs
+
+def topk_accuracy(score, label, k=1):
+    rank = score.argsort()
+    hit_top_k = [l in rank[i, -k:] for i, l in enumerate(label)]
+    accuracy = sum(hit_top_k) * 1.0 / len(hit_top_k)
+    return accuracy
+
+
+def weights_init(model):
+    classname = model.__class__.__name__
+    if classname.find('Conv1d') != -1:
+        model.weight.data.normal_(0.0, 0.02)
+        if model.bias is not None:
+            model.bias.data.fill_(0)
+    elif classname.find('Conv2d') != -1:
+        model.weight.data.normal_(0.0, 0.02)
+        if model.bias is not None:
+            model.bias.data.fill_(0)
+    elif classname.find('BatchNorm') != -1:
+        model.weight.data.normal_(1.0, 0.02)
+        model.bias.data.fill_(0)
