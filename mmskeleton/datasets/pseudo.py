6b1be664e29d76d15b9360ff100cce47ca11f910
sjyan <yysijie@gmail.com>
2019-08-29 16:58:26 +0800

    add description

--- mmskeleton/datasets/pseudo.py
+++ mmskeleton/datasets/pseudo.py
@@ -1,5 +1,5 @@
 class dataset():
     def __init__(self, data_path):
-        print('build dataset:')
+        print('build a pseudo dataset:')
         print('  data_path: {}'.format(data_path))
         print('')
