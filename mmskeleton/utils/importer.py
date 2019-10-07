fb14091251b9acdd2324d313cec1619008458d64
sjyan <yysijie@gmail.com>
2019-10-07 16:40:49 +0800

    change name to type

--- mmskeleton/utils/importer.py
+++ mmskeleton/utils/importer.py
@@ -1,49 +1,49 @@
 import sys
 
 
-def import_obj(name):
-    if not isinstance(name, str):
-        raise ImportError('Object name should be a string.')
+def import_obj(type):
+    if not isinstance(type, str):
+        raise ImportError('Object type should be string.')
 
-    # if name[0] == '.':
-    #     name = 'mmskeleton' + name
+    # if type[0] == '.':
+    #     type = 'mmskeleton' + type
 
-    mod_str, _sep, class_str = name.rpartition('.')
+    mod_str, _sep, class_str = type.rpartition('.')
     try:
         __import__(mod_str)
         return getattr(sys.modules[mod_str], class_str)
     except ModuleNotFoundError:
-        if name[0:11] != 'mmskeleton.':
-            return import_obj('mmskeleton.' + name)
+        if type[0:11] != 'mmskeleton.':
+            return import_obj('mmskeleton.' + type)
         raise ModuleNotFoundError('Object {} cannot be found in {}.'.format(
             class_str, mod_str))
 
 
-def call_obj(name, **kwargs):
-    if isinstance(name, str):
-        return import_obj(name)(**kwargs)
-    elif callable(name):
-        return name(**kwargs)
+def call_obj(type, **kwargs):
+    if isinstance(type, str):
+        return import_obj(type)(**kwargs)
+    elif callable(type):
+        return type(**kwargs)
     else:
-        raise ValueError('name should be string all callable.')
+        raise ValueError('type should be string all callable.')
 
 
-def set_attr(obj, name, value):
-    if not isinstance(name, str):
-        raise ImportError('Attribute name should be a string.')
+def set_attr(obj, type, value):
+    if not isinstance(type, str):
+        raise ImportError('Attribute type should be string.')
 
-    attr, _sep, others = name.partition('.')
+    attr, _sep, others = type.partition('.')
     if others == '':
         setattr(obj, attr, value)
     else:
         set_attr(getattr(obj, attr), others, value)
 
 
-def get_attr(obj, name):
-    if not isinstance(name, str):
-        raise ImportError('Attribute name should be a string.')
+def get_attr(obj, type):
+    if not isinstance(type, str):
+        raise ImportError('Attribute type should be string.')
 
-    attr, _sep, others = name.partition('.')
+    attr, _sep, others = type.partition('.')
     if others == '':
         return getattr(obj, attr)
     else:
